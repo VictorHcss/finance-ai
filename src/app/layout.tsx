f17a4a96e50b98a0b82b95d2a81be069819db2e7
VictorHcss <henvictor2@gmail.com>
2026-04-21 12:36:30 -0300

    feat: adiciona componente de sidebar lateral e estrutura de layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (\
-    <html lang="pt-BR">
+  return (
+    <html lang="pt-br">
       <body className="bg-zinc-950 text-zinc-50 flex h-screen overflow-hidden">
         <Sidebar />
         <div className="flex-1 overflow-y-auto">{children}</div>
